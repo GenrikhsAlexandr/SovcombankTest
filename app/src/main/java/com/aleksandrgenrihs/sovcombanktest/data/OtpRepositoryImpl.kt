55a7837a5427a41189b92ea3e13e325052ca4fdd
GenrikhsAlexandr <aleksandr.genrikhs@gmail.com>
2026-02-14 14:50:42 +0300

    fix OtpRepositoryImpl

--- app/src/main/java/com/aleksandrgenrihs/sovcombanktest/data/OtpRepositoryImpl.kt
+++ app/src/main/java/com/aleksandrgenrihs/sovcombanktest/data/OtpRepositoryImpl.kt
@@ -1,19 +1,22 @@
 package com.aleksandrgenrihs.sovcombanktest.data
 
+import com.aleksandrgenrihs.sovcombanktest.data.mapper.OtpRequestMapper
+import com.aleksandrgenrihs.sovcombanktest.data.mapper.OtpResendResponseMapper
+import com.aleksandrgenrihs.sovcombanktest.data.mapper.OtpVerifyResponseMapper
 import com.aleksandrgenrihs.sovcombanktest.domain.OtpRepository
+import com.aleksandrgenrihs.sovcombanktest.domain.model.OtpCode
 import com.aleksandrgenrihs.sovcombanktest.domain.model.OtpInfo
 import com.aleksandrgenrihs.sovcombanktest.domain.model.OtpVerify
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 class OtpRepositoryImpl @Inject constructor(
-//    private val service: ApiService,
+    private val service: ApiService,
     private val sharedPref: SharedPref,
-//    private val otpVerifyMapper: OtpVerifyResponseMapper,
-//    private val otpRequestMapper: OtpRequestMapper,
-//    private val otpResendMapper: OtpResendResponseMapper,
+    private val otpVerifyMapper: OtpVerifyResponseMapper,
+    private val otpRequestMapper: OtpRequestMapper,
+    private val otpResendMapper: OtpResendResponseMapper,
 ) : OtpRepository {
 
     override suspend fun canSendRequest(): Boolean {
@@ -37,14 +40,14 @@ class OtpRepositoryImpl @Inject constructor(
      */
     override suspend fun otpResend(): Result<OtpInfo> {
         return runCatching {
-//            val response = service.otpResend()
-//            val result = otpResendMapper.map(response)
+            val response = service.otpResend()
+            val result = otpResendMapper.map(response)
 
-            delay(2000)
-            val result = OtpInfo(
-                canResendIn = 60.seconds,
-                codeLength = 6
-            )
+//            delay(2000)
+//            val result = OtpInfo(
+//                canResendIn = 60.seconds,
+//                codeLength = 6
+//            )
             setCanResendIn(result.canResendIn, result.codeLength)
             result
         }
@@ -59,9 +62,9 @@ class OtpRepositoryImpl @Inject constructor(
      */
     override suspend fun otpVerify(code: String): Result<OtpVerify> {
         return runCatching {
-//            val request = otpRequestMapper.map(OtpCode(code))
-//            val response = service.otpVerify(request)
-//            otpVerifyMapper.map(response)
+            val request = otpRequestMapper.map(OtpCode(code))
+            val response = service.otpVerify(request)
+            otpVerifyMapper.map(response)
 
             delay(1000)
             OtpVerify(success = code == "123456")
